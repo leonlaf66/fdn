8f87b6a64abd107fc71824f20151d05b9c54dd96
LiChunHua <lichunhua@medatc.com>
2017-08-23 15:57:07 +0800

    x

---
+++ support/AppConfigs.php
@@ -0,0 +1,29 @@
+<?php
+namespace common\support;
+
+class AppConfigs extends \yii\db\ActiveRecord
+{
+    public static function tableName()  
+    {  
+        return 'app_configs';
+    }
+
+    public static function submit($appId, $configId, $configContent)
+    {
+        $config = self::get($appId, $configId);
+        if (!$config) {
+            $config = new self();
+            $config->app_id = $appId;
+            $config->config_id = $configId;
+        }
+        $config->config_content = $configContent;
+        return $config->save();
+    }
+
+    public static function get($appId, $configId)
+    {
+        return self::find()
+            ->where(['app_id' => $appId, 'config_id' => $configId])
+            ->one();
+    }
+}
