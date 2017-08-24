bfb1313ce8652f1d6fa2d6a0c314446a9d750632
LiChunHua <lichunhua@medatc.com>
2017-08-24 14:27:58 +0800

    x

--- estate/helpers/Config.php
+++ estate/helpers/Config.php
@@ -3,13 +3,13 @@ namespace common\estate\helpers;
 
 class Config 
 {
-    public static function get($group)
+    public static function get($group, $defValue = null)
     {
         $file = __DIR__.'/../config/'.$group.'.php';
         if (file_exists($file)) {
             return include(__DIR__.'/../config/'.$group.'.php');
         }
 
-        return null;
+        return $defValue;
     }
 }
