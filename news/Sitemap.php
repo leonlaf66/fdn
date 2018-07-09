36acdbc3cb966a447e9ba850af5a22b98ea49c3c
LiChunHua <lichunhua@medatc.com>
2018-07-09 11:23:40 +0800

    修改sitemap未进行areaId过滤的bug

--- news/Sitemap.php
+++ news/Sitemap.php
@@ -6,12 +6,13 @@ use common\helper\DbQuery;
 
 class Sitemap
 {
-    public static function map()
+    public static function map($areaId)
     {
         $query = (new \yii\db\Query())
             ->select('id, updated_at')
             ->from('news')
-            ->where('status=1')
+            ->where(['@>', 'area_id', '{'.strtolower($areaId).'}')
+            ->andWhere('status=1')
             ->orderBy(['updated_at' => 'DESC']);
 
         return $query->all();
--- yellowpage/Sitemap.php
+++ yellowpage/Sitemap.php
@@ -6,11 +6,12 @@ use common\helper\DbQuery;
 
 class Sitemap
 {
-    public static function map()
+    public static function map($areaId)
     {
         $query = (new \yii\db\Query())
             ->select('id')
             ->from('yellow_page')
+            ->where('area_id', strtolower($areaId))
             ->orderBy(['id' => 'DESC']);
 
         return $query->all();
